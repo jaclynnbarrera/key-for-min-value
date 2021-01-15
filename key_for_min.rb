f354cf7346fb643226fc7906774c114ba0d301d6
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-15 15:35:16 -0500

    commit tree fbf40eb7b6ab93bcaf167c75803843f9697af084

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+    smallest_value = 100000
+    final_key = nil
+    if name_hash.each do |k,v|
+        if v < smallest_value
+            smallest_value = v
+            final_key = k
+        end
+    end
+    end 
+    final_key
 end
